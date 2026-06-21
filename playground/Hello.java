154368382982323df75fa63757211abda9af293e
timothy adigun <adigun.timothy@gmail.com>
2026-06-21 20:16:39 +0100

    removed the final wordings

--- playground/Hello.java
+++ playground/Hello.java
@@ -1,8 +1,6 @@
+public class Hello {
 
-public final class Hello {
-
-    public final static void main(String[] args) {
+    public static void main(String[] args) {
         System.out.println("Start Here");
     }
 }
-
