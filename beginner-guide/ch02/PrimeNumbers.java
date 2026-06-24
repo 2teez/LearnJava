864c2489133e8212834d833fa63ad5070de118b6
timothy adigun <adigun.timothy@gmail.com>
2026-06-24 12:02:20 +0100

    calculate sqrt of a number once. Do the heavy job in few steps if possible.

--- beginner-guide/ch02/PrimeNumbers.java
+++ beginner-guide/ch02/PrimeNumbers.java
@@ -25,7 +25,8 @@ public class PrimeNumbers {
         if (number <= 1) {
             return false;
         }
-        for (int i = 2; i <= Math.sqrt(number); i++) {
+        var limit = (int) Math.sqrt(number);
+        for (int i = 2; i <= limit; i++) {
             if (number % i == 0) {
                 return false;
             }
