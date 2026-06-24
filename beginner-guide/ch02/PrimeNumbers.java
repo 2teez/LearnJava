507f875d02c392b68508ccf2eb7e2185b9e71f06
timothy adigun <adigun.timothy@gmail.com>
2026-06-24 11:24:49 +0100

    primt

---
+++ beginner-guide/ch02/PrimeNumbers.java
@@ -0,0 +1,37 @@
+package com.practice.primenumbers;
+
+import static java.lang.System.out;
+
+import java.util.Scanner;
+
+public class PrimeNumbers {
+
+    public static void main(String[] args) {
+        out.println("Start Here");
+        var userInput = new UserInput();
+        int number = userInput.getNumber();
+        boolean result = isPrime(number);
+        out.println("Is " + number + " a prime number? " + result);
+    }
+
+    public static boolean isPrime(int number) {
+        if (number <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    static class UserInput {
+
+        public int getNumber() {
+            var scanner = new Scanner(System.in);
+            out.print("Enter a number: ");
+            return scanner.nextInt();
+        }
+    }
+}
