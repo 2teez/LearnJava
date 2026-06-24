f030db7d9e9d04d0172c3464dc484d841d59f940
timothy adigun <adigun.timothy@gmail.com>
2026-06-24 12:45:36 +0100

    using throw error and other try/catch in java

--- beginner-guide/ch02/PrimeNumbers.java
+++ beginner-guide/ch02/PrimeNumbers.java
@@ -3,16 +3,26 @@ package com.practice.primenumbers;
 import static java.lang.System.out;
 
 import java.io.Console;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class PrimeNumbers {
 
+    private static final Scanner SCANNER = new Scanner(System.in);
+
     public static void main(String[] args) {
         out.println("Start Here");
-        var userInput = new UserInput();
-        int number = userInput.getNumber();
-        boolean result = isPrime(number);
-        out.println("Is " + number + " a prime number? " + result);
+        while (true) {
+            try {
+                var number = new UserInput().getNumber("Enter a number: ");
+                boolean result = isPrime(number);
+                out.println("Is " + number + " a prime number? " + result);
+                break;
+            } catch (InputMismatchException e) {
+                out.println("Error reading input: " + e.getMessage());
+                SCANNER.nextLine();
+            }
+        }
 
         Console console = System.console();
         if (console != null) {
@@ -36,10 +46,9 @@ public class PrimeNumbers {
 
     private static class UserInput {
 
-        private int getNumber() {
-            var scanner = new Scanner(System.in);
-            out.print("Enter a number: ");
-            return scanner.nextInt();
+        private int getNumber(String msg) {
+            out.print(msg);
+            return SCANNER.nextInt();
         }
     }
 }
