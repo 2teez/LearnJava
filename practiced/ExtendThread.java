9d8186fb0c96b7e57f6285567f7f8483323b2cf7
timothy adigun <adigun.timothy@gmail.com>
2026-07-03 14:18:45 +0100

    using a class to extend Thread and override run method

---
+++ practiced/ExtendThread.java
@@ -0,0 +1,31 @@
+package com.practice.extendthread;
+
+public class ExtendThread extends Thread {
+
+    public static void main(String[] args) {
+        System.out.println("Start Here");
+        ExtendThread thread = new ExtendThread();
+        thread.start();
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Main iteration " + i);
+            try {
+                Thread.sleep(400);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void run() {
+        System.out.println("Thread is running");
+        try {
+            for (int i = 0; i < 10; i++) {
+                System.out.println("Iteration " + i);
+                Thread.sleep(400);
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
