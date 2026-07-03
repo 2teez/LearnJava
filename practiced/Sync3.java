75872f8ac14672de7c207eca5451029555f1ef7e
timothy adigun <adigun.timothy@gmail.com>
2026-07-03 21:55:23 +0100

    using thread and synchronization to sum up values

---
+++ practiced/Sync3.java
@@ -0,0 +1,90 @@
+package com.practice.sync3;
+
+public class Sync3 {
+
+    public static void main(String[] args) {
+        // using partially summed values and thread to sum an array values
+        // synchronizing on the same object to avoid race conditions
+        int[] newArrays = { 2, 8, 2, 5, 7, 3, 1 };
+        var thread3 = SumArrayThread.createAndStartThread(newArrays, 0, 4);
+        var thread4 = SumArrayThread.createAndStartThread(newArrays, 4, 7);
+        try {
+            thread3.join();
+            thread4.join();
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+        // System.out.println("Sum: " + (thread3.getSum() + thread4.getSum()));
+        System.out.println("Sum: " + SumArrayThread.getSum());
+    }
+}
+
+class SumArray {
+
+    public int sumPartially(int[] arr, int start, int end) {
+        int sum = 0;
+        System.out.println(
+            Thread.currentThread().getName() +
+                " called with start=" +
+                start +
+                " stop=" +
+                end
+        );
+        for (int i = start; i < end; i++) {
+            sum += arr[i];
+            System.out.println(
+                Thread.currentThread().getName() +
+                    " sum=" +
+                    sum +
+                    " i=" +
+                    arr[i]
+            );
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+        return sum;
+    }
+}
+
+class SumArrayThread implements Runnable {
+
+    private final int start;
+    private final int stop;
+    private static int sum;
+    private final Thread thread;
+    private final int[] arr;
+    private static final SumArray sumArray = new SumArray();
+
+    private SumArrayThread(int[] arr, int start, int stop) {
+        this.arr = arr;
+        this.start = start;
+        this.stop = stop;
+        this.thread = new Thread(this);
+    }
+
+    public static Thread createAndStartThread(int[] arr, int start, int stop) {
+        var myThread = new SumArrayThread(arr, start, stop);
+        myThread.thread.start();
+        return myThread.thread;
+    }
+
+    @Override
+    public void run() {
+        synchronized (sumArray) {
+            sum += sumArray.sumPartially(arr, start, stop);
+            System.out.println(
+                Thread.currentThread().getName() +
+                    ": sumArrayThread finished" +
+                    " sum: " +
+                    sum
+            );
+        }
+    }
+
+    public static int getSum() {
+        return sum;
+    }
+}
