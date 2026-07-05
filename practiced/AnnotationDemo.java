5ba3f169d069b462c800bbf51b766c2115d64d33
timothy adigun <adigun.timothy@gmail.com>
2026-07-05 10:17:06 +0100

    a little change to annotation in java practice file

--- practiced/AnnotationDemo.java
+++ practiced/AnnotationDemo.java
@@ -1,10 +1,11 @@
-
 package com.practice.annotationdemo;
 
+import com.practice.version.Version;
+
+@Version(major = 1, minor = 0)
 public class AnnotationDemo {
 
     public static void main(String[] args) {
         System.out.println("Start Here");
     }
 }
-
