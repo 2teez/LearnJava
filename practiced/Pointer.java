04f8cee8c99e16d6bc6f164c545d8577fc946d96
timothy adigun <adigun.timothy@gmail.com>
2026-07-22 11:20:03 +0100

    pointer pratice in java

--- practiced/Pointer.java
+++ practiced/Pointer.java
@@ -9,6 +9,9 @@ class Pointer {
         out.println(p);
         Point pt = new Point(3, 4).translate(1, 3).scale(0.5);
         out.println(pt);
+        // using POJ
+        var pt2 = new PointCl(3, 4).translate(1, 3).scale(0.5);
+        out.println(pt2);
     }
 }
 
@@ -19,5 +22,32 @@ record Point(double x, double y) {
 
     Point scale(double value) {
         return new Point(x * value, y * value);
+    }
+}
+
+class PointCl {
+    private double x;
+    private double y;
+
+    PointCl(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    PointCl translate(int xValue, int yValue) {
+        this.x += xValue;
+        this.y += yValue;
+        return this;
+    }
+
+    PointCl scale(double value) {
+        this.x *= value;
+        this.y *= value;
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("PointCl(x=%.1f,y=%.1f)", this.x, this.y);
     }
 }
