36d0f5dc4afce9950d08efd145f375b2642e8915
timothy adigun <adigun.timothy@gmail.com>
2026-07-22 10:44:02 +0100

    oop practice

--- practiced/Pointer.scala
+++ practiced/Pointer.scala
@@ -1,4 +1,12 @@
 package com.progscala3.pointer
 
+case class Point(x: Double, y: Double):
+  def translate(xVal: Int, yVal: Int): Point = Point(x + xVal, y + yVal)
+
+  def scale(value: Double): Point = Point(x * value, y * value)
+
 @main def main(args: String*): Unit =
-  println("Start, Here!")
+  val p = Point(3, 4);
+  println(p);
+  val pt = Point(3, 4).translate(1, 3).scale(0.5);
+  println(pt);
