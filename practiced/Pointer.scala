1554db3d15cf9d96edc1206aa483c7f02307f4aa
timothy adigun <adigun.timothy@gmail.com>
2026-07-22 11:13:59 +0100

    pointer practice in scala

--- practiced/Pointer.scala
+++ practiced/Pointer.scala
@@ -5,8 +5,17 @@ case class Point(x: Double, y: Double):
 
   def scale(value: Double): Point = Point(x * value, y * value)
 
+class PointCl(x: Double, y: Double):
+  def translate(xVal: Int, yVal: Int): PointCl = PointCl(x + xVal, y + yVal)
+  def scale(value: Double): PointCl = PointCl(x * value, y * value)
+  override def toString: String = s"PointCl(x=$x,y=$y)"
+
+object PointCl:
+  def apply(x: Double, y: Double): PointCl = new PointCl(x, y)
+
 @main def main(args: String*): Unit =
   val p = Point(3, 4);
   println(p);
-  val pt = Point(3, 4).translate(1, 3).scale(0.5);
+  val pt = Point(3, 4).translate(1, 3).scale(0.5)
   println(pt);
+  println(PointCl(3, 4).translate(1, 3).scale(0.5))
