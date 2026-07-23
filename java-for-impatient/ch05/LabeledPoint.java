493a0056f2926186bc2f3d1086d4c8f024b56b6a
timothy adigun <adigun.timothy@gmail.com>
2026-07-23 23:57:19 +0100

    labeledpoint which is a subclass in java

---
+++ java-for-impatient/ch05/LabeledPoint.java
@@ -0,0 +1,17 @@
+package com.practice.labeledpoint;
+
+import com.practice.point.Point;
+
+public class LabeledPoint extends Point {
+
+    private String label;
+
+    public LabeledPoint(String label, double x, double y) {
+        super(x, y);
+        this.label = label;
+    }
+
+    public String getLabel() {
+        return this.label;
+    }
+}
