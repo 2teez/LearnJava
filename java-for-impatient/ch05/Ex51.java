b59c5485fc1b426197a451fcdc064871d45ebe19
timothy adigun <adigun.timothy@gmail.com>
2026-07-23 23:57:43 +0100

    main class for inheritance in java

--- java-for-impatient/ch05/Ex51.java
+++ java-for-impatient/ch05/Ex51.java
@@ -1,8 +1,18 @@
 package com.practice.ex51;
 
+import static java.lang.System.out;
+
+import com.practice.circle.Circle;
+import com.practice.point.Point;
+import com.practice.shape.Shape;
+
 public class Ex51 {
 
     public static void main(String[] args) {
-        System.out.println("Start Here");
+        Point p = new Point(120, 80);
+        out.println(p);
+        Circle circle = new Circle(p, 20);
+        circle.moveBy(1, 0);
+        System.out.println(circle.getCenter());
     }
 }
