4f561b75402a51436c7779cc0e8668c4954d7fd1
timothy adigun <adigun.timothy@gmail.com>
2026-07-25 00:45:21 +0100

    use method from java with exception

--- java-for-impatient/ch06/Eh.java
+++ java-for-impatient/ch06/Eh.java
@@ -2,10 +2,12 @@ package com.practice.eh;
 
 import static java.lang.System.Logger.Level.ERROR;
 
+import java.io.Console;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Eh {
@@ -16,14 +18,24 @@ public class Eh {
 
     public static void main(String[] args) {
         try {
-            System.out.println(new Eh().sumOfValues("doubleFiles.txt"));
-        } catch (IOException e) {
+            String input = getUserInput("Enter a filename: ");
+            System.out.println(new Eh().sumOfValues(input));
+        } catch (IllegalStateException | IOException e) {
             logger.log(ERROR, e.getMessage(), e);
         }
     }
 
+    public static String getUserInput(String msg) throws IllegalStateException {
+        System.out.print(msg);
+        Console console = System.console();
+        if (console == null) {
+            throw new IllegalStateException("No console available");
+        }
+        return console.readLine();
+    }
+
     public double sumOfValues(String filename) throws IOException {
-        ArrayList<Double> values = readValues(filename);
+        List<Double> values = readValues(filename);
         double sum = 0;
         for (double value : values) {
             sum += value;
@@ -31,19 +43,14 @@ public class Eh {
         return sum;
     }
 
-    public ArrayList<Double> readValues(String filename) throws IOException {
-        ArrayList<Double> values = new ArrayList<>();
+    public List<Double> readValues(String filename) throws IOException {
+        List<Double> values = new ArrayList<>();
         try (Scanner scanner = new Scanner(new FileReader(filename))) {
             while (scanner.hasNextDouble()) {
                 values.add(scanner.nextDouble());
             }
         } catch (FileNotFoundException e) {
             throw new IOException("File not found: " + filename, e);
-        } catch (NumberFormatException e) {
-            throw new IOException(
-                "Number format exception: " + e.getMessage(),
-                e
-            );
         }
 
         return values;
